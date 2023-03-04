2254c06a20c9574ae6a510f7657b3ad28cd03837
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2023-03-04 12:23:37 +0300

    Matrix.kt

--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -1,3 +1,12 @@
-class Matrix {
-    
+class Matrix(arr: Collection<DoubleArray>): ArrayList<DoubleArray>(arr) {
+    constructor(vararg lines: DoubleArray) : this(lines.map { it })
+
+    init {
+        /* TODO: check sizes */
+    }
+
+    /** Matrix x Matrix multiplication **/
+    fun multiply(other: Matrix): Matrix {
+        TODO()
+    }
 }
