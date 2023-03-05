102909c5caffa280cbf369bd662f9e7aad112cf5
Anna <0301annaumnova@gmail.com>
2023-03-05 20:33:53 +0300

    //

--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -1,12 +1,38 @@
-class Matrix(arr: Collection<DoubleArray>): ArrayList<DoubleArray>(arr) {
-    constructor(vararg lines: DoubleArray) : this(lines.map { it })
+class Matrix(arr: Collection<DoubleArray>) : ArrayList<DoubleArray>(arr) {
+    // constructor(vararg lines: DoubleArray) : this(lines.map { it })
+
+    private val columns = this[0].size //width
+    private val rows = this.size //height
 
     init {
-        /* TODO: check sizes */
+        if (this.isEmpty()) error("It mustn't be empty")
+        for (i in this.indices) {
+            if (columns != this[i].size)
+                throw IllegalArgumentException("Wrong size!")
+        }
     }
 
     /** Matrix x Matrix multiplication **/
-    fun multiply(other: Matrix): Matrix {
-        TODO()
+    fun multiply(b: Matrix): Matrix {
+        val result = List(rows) { Array(columns) { .0 }.toDoubleArray() }
+        for (i in result.indices) {
+            for (j in 0 until this.rows) {
+                for (k in 0 until this.columns) {
+                    result[i][j] += this[i][k] * b[k][j]
+                }
+            }
+        }
+        return Matrix(result)
     }
+
+    override fun toString(): String { //to string
+        var result = ""
+        var ans: String
+        for (i in this.indices) {
+            ans = this[i].contentToString()
+            result += ans
+        }
+        return result
+    }
+
 }
